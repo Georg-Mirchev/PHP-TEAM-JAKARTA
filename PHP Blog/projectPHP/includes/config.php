4ac341ab97616f593065e33a5cf6798e756768e6
Pe6o601 <petermarinov93@abv.bg>
2014-12-15 13:57:59 +0200

    made projectPHP with config file with db

---
+++ PHP Blog/projectPHP/includes/config.php
@@ -0,0 +1,26 @@
+<?php
+ob_start();
+session_start();
+
+define('DBHOST','http://pmarinov.cloudvps.bg/');
+define('DBUSER','pmaruq7f');
+define('DBPASS','by+Jch]92JTV');
+define('DBNAME','pmaruq7f_db');
+$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+date_default_timezone_set('Europe/Sofia');
+
+function __load($class){
+    $class = strtolower($class);
+    $classpath = 'classes/class.'.$class . '.php';
+    if ( file_exists($classpath)) {
+        require_once $classpath;
+    }
+    $classpath = '../classes/class.'.$class . '.php';
+    if ( file_exists($classpath)) {
+        require_once $classpath;
+    }
+
+}
+$user = new User($db);
+?>
